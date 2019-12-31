fb6ca00e2852c13b1f3e88658d10bf56561c9d67
Mateus-M-Reis <mateusmreis92@gmail.com>
2019-12-31 03:51:08 -0300

    Created Repository From My Private Bootstrap 4 Template

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    $("#my-carousel").carousel( { interval: 2000 } );
+    
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#my-carousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#my-carousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
+$('#loginButton').click(function(){
+    $('#loginModal').modal( 'show' )
+});
+$('#reserveButton').click(function(){
+    $('#reserveModal').modal( 'show' )
+});
